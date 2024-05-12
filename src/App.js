956398030385c03d8b97b1f0a23aa8a34b4dd328
Ian Brennan <ian@convertkit.com>
2024-05-12 10:12:18 -0400

    streaming example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    if (!inputText.trim()) return;
+    const userMessage = { text: inputText, isBot: false };
+    const body = {
+      question: inputText,
+      namespace: "prod-science",
+      character: "steve_brule",
+      chat_session_id: "SB2",
+    };
+
+    const botMessage = { text: "", isBot: true };
+
+    setMessages([...messages, userMessage, botMessage]);
+    setInputText("");
+    const response = await fetch("https://quizzical-nlbh.onrender.com/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.body) return;
+
+    let decoder = new TextDecoderStream();
+
+    const reader = response.body.pipeThrough(decoder).getReader();
+
+    let accumulatedAnswer = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      accumulatedAnswer += value;
+      setMessages((currentMessages) => {
+        const updatedMessages = [...currentMessages];
+        const lastMessageIndex = updatedMessages.length - 1;
+
+        updatedMessages[lastMessageIndex] = {
+          ...updatedMessages[lastMessageIndex],
+          text: accumulatedAnswer,
+        };
+
+        return updatedMessages;
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="font-semibold text-4xl text-green-900">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Quizzical</h1>
+        <div>
+          {messages.map((message, index) => (
+            <div
+              key={`message-${index}`}
+              className={`text-white ${
+                message.isBot ? "bg-green-900" : "bg-blue-500"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+
+        <form onSubmit={sendMessage}>
+          <input
+            placeholder="Ask me about your subject!..."
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 }
