1b06d49070f2fee1634270ef577547b895027851
Ian Brennan <ian@convertkit.com>
2024-05-14 18:56:08 -0400

    basic style update

--- src/App.js
+++ src/App.js
@@ -86,8 +86,8 @@ function App() {
     <div className="App">
       <div>
         <div className="text-4xl text-slate-700">Quizzical</div>
-        <div className="flex w-full justify-center">
-          <div className="flex flex-col gap-3 w-3/4">
+        <div className="flex w-full">
+          <div className="flex flex-col gap-3 w-full p-3 rounded-md">
             {messages.map((message, index) => (
               <div key={`message-${index}`} className="flex">
                 {!message.isBot && <div className="w-1/4" />}
@@ -100,7 +100,7 @@ function App() {
                   <div
                     className={`${
                       message.isBot ? "bg-blue-100" : "bg-blue-500"
-                    } w-fit rounded-full p-3 text-center shadow-lg shadow-slate-300`}
+                    } w-fit rounded-lg py-3 px-4 text-left shadow-lg shadow-slate-300`}
                   >
                     {message.text}
                   </div>
@@ -108,21 +108,20 @@ function App() {
               </div>
             ))}
             <div ref={messagesEndRef} />
-          </div>
-        </div>
-
-        <div className="mt-3">
-          <form onSubmit={sendMessage}>
-            <div className="w-full flex justify-center">
-              <input
-                placeholder="Ask me about your subject!..."
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                className="w-3/4 m-auto border-2 border-blue-500 p-2 rounded-full shadow-lg shadow-slate-300"
-              />
+            <div className="mt-3">
+              <form onSubmit={sendMessage}>
+                <div className="w-full flex justify-center">
+                  <input
+                    placeholder="Ask me about your subject!..."
+                    type="text"
+                    value={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
+                    className="w-3/4 m-auto border-2 border-blue-500 p-2 rounded-lg shadow-lg shadow-slate-300"
+                  />
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
