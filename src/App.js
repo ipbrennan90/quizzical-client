b4004c99bb608fa6fd394425642bce01d7195bc1
Ian Brennan <ian@convertkit.com>
2024-05-14 09:24:59 -0400

    Styling-demoable-v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import "./App.css";
 import React, { useEffect, useRef, useState } from "react";
 
+async function getSessionId(chatSessionId) {
+  const body = {
+    chat_session_id: chatSessionId,
+  };
+
+  const response = await fetch("http://127.0.0.1:5000/session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const response_body = await response.json();
+  return response_body.data.chat_session_id;
+}
+
 function App() {
   const [messages, setMessages] = useState([]);
+  const [chatSessionId, setChatSessionId] = useState(null);
   const [inputText, setInputText] = useState("");
   const messagesEndRef = useRef(null);
   const scrollToBottom = () => {
@@ -14,19 +32,21 @@ function App() {
   const sendMessage = async (event) => {
     event.preventDefault();
     if (!inputText.trim()) return;
+    const currentChatSessionId = await getSessionId(chatSessionId);
+    setChatSessionId(currentChatSessionId);
     const userMessage = { text: inputText, isBot: false };
     const body = {
       question: inputText,
       namespace: "prod-science",
       character: "steve_brule",
-      chat_session_id: "SB2",
+      chat_session_id: currentChatSessionId,
     };
 
     const botMessage = { text: "", isBot: true };
 
     setMessages([...messages, userMessage, botMessage]);
     setInputText("");
-    const response = await fetch("https://quizzical-nlbh.onrender.com/chat", {
+    const response = await fetch("http://127.0.0.1:5000/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -65,29 +85,45 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Quizzical</h1>
-        <div>
-          {messages.map((message, index) => (
-            <div
-              key={`message-${index}`}
-              className={`text-white ${
-                message.isBot ? "bg-green-900" : "bg-blue-500"
-              }`}
-            >
-              {message.text}
-            </div>
-          ))}
-          <div ref={messagesEndRef} />
+        <div className="text-4xl text-slate-700">Quizzical</div>
+        <div className="flex w-full justify-center">
+          <div className="flex flex-col gap-3 w-3/4">
+            {messages.map((message, index) => (
+              <div key={`message-${index}`} className="flex">
+                {!message.isBot && <div className="w-1/4" />}
+                <div
+                  key={`message-${index}`}
+                  className={`flex w-3/4 text-slate-700 ${
+                    message.isBot ? "justify-start" : "justify-end"
+                  }`}
+                >
+                  <div
+                    className={`${
+                      message.isBot ? "bg-blue-100" : "bg-blue-500"
+                    } w-fit rounded-full p-3 text-center shadow-lg shadow-slate-300`}
+                  >
+                    {message.text}
+                  </div>
+                </div>
+              </div>
+            ))}
+            <div ref={messagesEndRef} />
+          </div>
         </div>
 
-        <form onSubmit={sendMessage}>
-          <input
-            placeholder="Ask me about your subject!..."
-            type="text"
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
-        </form>
+        <div className="mt-3">
+          <form onSubmit={sendMessage}>
+            <div className="w-full flex justify-center">
+              <input
+                placeholder="Ask me about your subject!..."
+                type="text"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+                className="w-3/4 m-auto border-2 border-blue-500 p-2 rounded-full shadow-lg shadow-slate-300"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
